64e6e4b88ee27bf2ccf206287b79bf678db50f18
eunseo <2023180012@tukorea.ac.kr>
2025-12-02 15:04:46 +0900

    -1: 게임 오버, 4: 게임 클리어

--- CG_Project/main.cpp
+++ CG_Project/main.cpp
@@ -32,13 +32,14 @@ GLuint tex_icon_player;
 GLuint tex_GAMECLEAR_1;
 GLuint tex_GAMECLEAR_2;
 GLuint tex_boss_died;
+GLuint tex_player_smile;
 // game over texture
 GLuint tex_GAMEOVER_1;
 GLuint tex_GAMEOVER_2;
 GLuint tex_boss_smile;
 
 Player player; // player object(temp)
-int currentStage = -1;   // current stage 0: title, 1, 2, 3 -1: gameover, 4: gameclear
+int currentStage = 4;   // current stage 0: title, 1, 2, 3 -1: gameover, 4: gameclear
 
 std::vector<Bullet> bullets;  // bullet objects
 
@@ -576,6 +577,7 @@ int main(int argc, char** argv)
 	tex_GAMECLEAR_1 = LoadTexture("CG_GameClear_1.png"); // game over texture load
 	tex_GAMECLEAR_2 = LoadTexture("CG_GameClear_2.png");
 	tex_boss_died = LoadTexture("boss_died.png");
+	tex_player_smile = LoadTexture("game_clear_airplane.png");
 	tex_GAMEOVER_1 = LoadTexture("CG_GameOver_1.png"); // game over texture load
 	tex_GAMEOVER_2 = LoadTexture("CG_GameOver_2.png");
 	tex_boss_smile = LoadTexture("boss_smile.png");
@@ -894,6 +896,11 @@ GLvoid drawScene()
 		model_3 = glm::translate(model_3, glm::vec3(0.0f, -8.0f, -10.0f));
 		model_3 = glm::scale(model_3, glm::vec3(45.0f, 45.0f, 0.01f));
 		DrawBossCube(bossShaderProgramID, cubeVAO, tex_boss_died, model_3, cameraPos, lightPos, vTransform, pTransform);
+
+		glm::mat4 model_4 = glm::mat4(1.0f);
+		model_4 = glm::translate(model_4, glm::vec3(11.0f, -9.0f, -9.0f));
+		model_4 = glm::scale(model_4, glm::vec3(50.0f, 30.0f, 0.01f));
+		DrawBossCube(bossShaderProgramID, cubeVAO, tex_player_smile, model_4, cameraPos, lightPos, vTransform, pTransform);
 	}
 
 	if (currentStage == -1)
